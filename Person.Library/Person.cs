5bd0c3934f3b342ad6bd4a311f1094061cb8db0f
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-31 15:06:44 -0400

    first commit

---
+++ Person.Library/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persons.Library
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private string Address { get; set; }
+
+        public Person(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+        public override string ToString()
+        {
+            return ($"{ GetAddress()}, {GetName()}");
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetAddress()
+        {
+            return Address;
+        }
+    }
+
+    
+}
